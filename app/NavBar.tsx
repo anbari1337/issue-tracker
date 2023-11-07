82c85c3d760a10cc44a38f3d6e34c4698786064d
Amine Anbari <amine.a@wiggli.io>
2023-11-07 15:45:04 +0100

    first commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import cx from "classnames";
+
+const links = [
+  {
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    label: "Issues",
+    href: "/issues",
+  },
+];
+const NavBar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className='flex space-x-6 mb-8 px-5 h-14 border-b items-center'>
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
+      <ul className='flex space-x-6'>
+        {links.map((link) => (
+          <li
+            key={link.href}
+            className={cx("hover:text-zinc-800 transition-colors", {
+              "text-zinc-900": pathname === link.href,
+              "text-zinc-500": pathname !== link.href,
+            })}
+          >
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;

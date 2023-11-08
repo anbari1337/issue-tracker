af4bd91f3ac948ea8190830965699302eabb9734
Amine Anbari <amine.a@wiggli.io>
2023-11-08 14:25:18 +0100

    styling issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { notFound } from "next/navigation";
 import { getIssueById } from "../api/issues";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import Markdown from "react-markdown";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const issue = await getIssueById(params.id);
@@ -8,9 +11,14 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.createdAT.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap='2' my='2'>
+        <IssueStatusBadge status={issue.status} />
+        <Text className='text-gray-500'>{issue.createdAT.toDateString()}</Text>
+      </Flex>
+      <Card className='prose mt-5'>
+        <Markdown>{issue.description}</Markdown>
+      </Card>
     </div>
   );
 }
